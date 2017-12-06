c8abfceb4de67c76e3cc04d95bb4d0369ce1edc6
Alex <contact@alexanderbell.info>
2017-12-06 11:48:42 -0800

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,26 @@
+// Scripts go here
+var imagesLoaded = require('imagesloaded');
+var MirrorFx = require('./mirrorFx.js');
+var Slideshow = require('./slideshow.js');
+
+(function() {
+    function init() {
+        new Slideshow(document.querySelector('.slideshow'));
+    }
+    
+    // Fake loading time..
+    var startTime = new Date().getTime();
+    imagesLoaded(document.querySelector('main'), function() {
+        var elapsed = new Date().getTime() - startTime,
+            initFn = function() {
+                document.body.classList.remove('loading');
+                setTimeout(init, 50);
+            };
+        if( elapsed > 2000) {
+            initFn();
+        }
+        else {
+            setTimeout(initFn, 1000);
+        }
+    });
+}());
